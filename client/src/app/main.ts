b36a112bfb48f1d8d619f5f514f19def1f0b6eba
leo <kuzmichleva1@gmail.com>
2025-10-10 17:43:04 +0200

    create empty dashboard page and protect it from unauthenticated users

--- client/src/app/main.ts
+++ client/src/app/main.ts
@@ -3,11 +3,17 @@ import "./style.css";
 import App from "./App.vue";
 import appRouter from "./router";
 import { createPinia } from "pinia";
+import { useAuthStore } from "@/entities/user";
 
 const pinia = createPinia();
 const app = createApp(App);
 
 app.use(appRouter);
 app.use(pinia);
+
+appRouter.beforeEach((to) => {
+    const authStore = useAuthStore(pinia);
+    if (to.meta.requiresAuth && !authStore.isAuthenticated) return "/login";
+});
 
 app.mount("#app");
