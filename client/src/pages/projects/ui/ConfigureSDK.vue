0a11791524ccfb086f3f927069fb0f8d9fbcd2f6
Leo <kuzmichleo2@gmail.com>
2025-10-15 08:59:17 +0000

    start working on getting-started button

--- client/src/pages/projects/ui/ConfigureSDK.vue
+++ client/src/pages/projects/ui/ConfigureSDK.vue
@@ -1,55 +1,70 @@
 <script setup lang="ts">
-import { useRoute } from "vue-router";
+import { useRoute, RouterLink } from "vue-router";
+import { SmallButton } from "@/shared/ui";
+import { useProjectStor, getProjectByName } from "@/entities/project";
 
 const route = useRoute();
+const projectStore = useProjectStore();
+
+const applyProject = () => {
+  console.log(route.params.name);
+  const project = getProjectByName(route.params.name);
+  projectStore.setCurrentProject(project);
+  console.log(projectStore.currentProject);
+};
 </script>
 
 <template>
-    <div class="flex flex-col">
-        <h2 class="mt-10 font-bold text-2xl">Configure SDK</h2>
-        <p class="text-sm my-4">
-            Initialize Sentry as early as possible in your application's
-            lifecycle, usually your Vue app's entry point (
-            <span class="text-red-600">main.ts/js</span>
-            ).
-        </p>
+  <div class="flex flex-col">
+    <h2 class="mt-10 font-bold text-2xl">Configure SDK</h2>
+    <p class="text-sm my-4">
+      Initialize Sentry as early as possible in your application's lifecycle,
+      usually your Vue app's entry point (
+      <span class="text-red-600">main.ts/js</span>
+      ).
+    </p>
 
-        <div class="bg-gray-800 text-white px-5 py-3 rounded-md">
-            <div class="mb-2 font-semibold text-secondary">
-                <span>JavaScript</span>
-            </div>
-            <code class="">
-                <span class="text-red-600">import</span> { createApp }
-                <span class="text-red-600">from</span> "vue";<br />
-                <span class="text-red-600">import</span> { createRouter }
-                <span class="text-red-600">from</span> "vue-router";<br />
-                <span class="text-red-600">import</span> router
-                <span class="text-red-600">from</span> "./router";<br /><br />
-                <span class="text-red-600">import</span> * as Sentry
-                <span class="text-red-600">from</span>
-                "@sentry/vue";<br /><br />
-                const app = <span class="text-blue-400">createApp</span>({
-                <span class="text-secondary">// ...</span> });<br /><br />
-                Sentry.<span class="text-blue-400">init</span>({<br />
-                app,<br />
-                <span class="text-blue-400">dsn</span>: "{{
-                    route.params.name
-                }}",<br />
-                <span class="text-secondary"
-                    >// Setting this option to true will send default PII data
-                    to Sentry.</span
-                >
-                <br />
-                <span class="text-secondary"
-                    >// For example, automatic IP address collection on
-                    events</span
-                ><br />
-                <span class="text-blue-400">sendDefaultPii</span>: true<br />
-                });<br /><br />
+    <div class="bg-gray-800 text-white px-5 py-3 rounded-md">
+      <div class="mb-2 font-semibold text-secondary">
+        <span>JavaScript</span>
+      </div>
+      <code class="">
+        <span class="text-red-600">import</span> { createApp }
+        <span class="text-red-600">from</span> "vue";<br />
+        <span class="text-red-600">import</span> { createRouter }
+        <span class="text-red-600">from</span> "vue-router";<br />
+        <span class="text-red-600">import</span> router
+        <span class="text-red-600">from</span> "./router";<br /><br />
+        <span class="text-red-600">import</span> * as Sentry
+        <span class="text-red-600">from</span>
+        "@sentry/vue";<br /><br />
+        const app = <span class="text-blue-400">createApp</span>({
+        <span class="text-secondary">// ...</span> });<br /><br />
+        Sentry.<span class="text-blue-400">init</span>({<br />
+        app,<br />
+        <span class="text-blue-400">dsn</span>: "{{ route.params.name }}",<br />
+        <span class="text-secondary"
+          >// Setting this option to true will send default PII data to
+          Sentry.</span
+        >
+        <br />
+        <span class="text-secondary"
+          >// For example, automatic IP address collection on events</span
+        ><br />
+        <span class="text-blue-400">sendDefaultPii</span>: true<br />
+        });<br /><br />
 
-                app.<span class="text-blue-400">use</span>(router);<br />
-                app.<span class="text-blue-400">mount</span>("#app");
-            </code>
-        </div>
+        app.<span class="text-blue-400">use</span>(router);<br />
+        app.<span class="text-blue-400">mount</span>("#app");
+      </code>
     </div>
+
+    <div class="mt-5">
+      <RouterLink to="/issues">
+        <SmallButton @click="applyProject" color="purple">
+          Take me to issues
+        </SmallButton>
+      </RouterLink>
+    </div>
+  </div>
 </template>
